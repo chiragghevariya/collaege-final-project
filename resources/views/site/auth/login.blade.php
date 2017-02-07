a1996c1fc71d7d0948ad40bb44e395fa7c91bb7e
chiragghevariya <gcb1196@gmail.com>
2017-02-07 15:00:42 +0530

    some changed

--- resources/views/site/auth/login.blade.php
+++ resources/views/site/auth/login.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
+    <div style="width: 83%;margin:auto;margin-top: 3em" class="pull-right">
+
 
         <div class="container">
             <div class="row">
@@ -11,30 +13,36 @@
                             {{ csrf_field() }}
                             <div class="card-content">
                                 <span class="card-title">
-                                    Login Here  
+                                    Login Here
                                 </span>
-                                
+
                                 <div class="input-field">
                                     <input type="text" name="email" placeholder="user@domain.com">
                                     <label class="active" for="">Email</label>
                                 </div>
-                                    
+
                                 <div class="input-field">
                                     <input type="password" name="password" placeholder="">
                                     <label class="active" for="">Password</label>
                                 </div>
 
-                                                                
+
                             </div>
                             <div class="card-action">
-                                  <input type="submit" class="btn grey" value="Login">
-                                  <p>Need To Register ? <a href=#> Click Here </a> </p>   
+                                <input type="submit" class="btn grey" value="Login">
+                                <p>Need To Register ? <a href=#> Click Here </a> </p>
                             </div>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
+
+
+
+
+    </div>
+
 
 
     @stop
